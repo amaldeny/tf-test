b21293773a9a832ba866db0bf56f62e14945b43b
amaldeny <amal.deny@rackspace.com>
2021-06-01 12:56:57 +0530

    added new variables and tfvars file

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project_id   = "ops-amal-deny"
+region1      = "asia-south1"
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "project_id" {
   description = "The ID of the project where this VPC will be created"
 }
+
+variable "region1" {
+  description = "The region where this VPC will be created"
+}
